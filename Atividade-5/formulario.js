f53e562d4dc82215b6fa323574767f0d9a0a6379
Gustavo Diniz <dinizgustavo085@gmail.com>
2026-06-17 21:49:58 -0300

    atividade js

---
+++ Atividade-5/formulario.js
@@ -0,0 +1,25 @@
+
+function cracharvirtual(){
+    window.alert("Seu formulario foi enviado ")
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value
+    let cargo = document.getElementById("cargo").value
+    let departamento = document.getElementById("departamento").value
+    const acesso = document.getElementById
+    
+
+    console.log("Seu nome é: ",nome)
+    console.log("Sua idade: ",idade )
+    console.log("Seu Cargo: ",cargo)
+    console.log("Seu departamento: ", departamento)
+ 
+    alert(
+        "Funcionario Cadastrado  \n \n Nome do funcionario:  "+ nome +  
+        " \n idade funcionario: "+ idade +  
+        "\n Cargo funcionario : "+ cargo +  
+        "\n Departamento funcionario: "+ departamento  
+    )
+
+
+}
+
