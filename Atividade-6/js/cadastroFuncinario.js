db695a7b5bb492812f2220dff4d8946656ca5e4f
Gustavo Diniz <dinizgustavo085@gmail.com>
2026-06-22 20:32:09 -0300

    adicionando atividade 6

---
+++ Atividade-6/js/cadastroFuncinario.js
@@ -0,0 +1,31 @@
+function cadastrofuncionario(){
+    let nome = document.getElementById("nome-funcionario").value;
+    let hora = document.getElementById("horas-funcionario").value;
+    let cargo = document.getElementById("cargo-funcionario").value;
+
+    if (nome.trim() === "" || cargo.trim()  === "" ){
+        alert("Por favor, coloque alguma informação");
+        return;
+    }
+
+    console.log("Cadastando", nome, hora, cargo)
+
+    let sessaoLista = document.getElementById("lista-funcionarios");
+    let cardFuncionario = document.createElement("div");
+
+    cardFuncionario.style.border = "1px solid #ccc";
+    cardFuncionario.style.padding = "10px";
+    cardFuncionario.style.margin = "10px 0";
+    cardFuncionario.style.borderRadius = "5px";
+
+    cardFuncionario.innerHTML =
+        "<p><strong>Nome:</strong> " + nome + "</p>" +
+        "<p><strong>Cargo:</strong> " + cargo + "</p>" +
+        "<p><strong>Carga Horária:</strong> " + hora + "h semanais</p>";
+
+    sessaoLista.appendChild(cardFuncionario);
+
+    document.getElementById(nome).value = "";
+    document.getElementById(cargo).value = "";
+    document.getElementById(hora).selectedIndex = 0;
+}
