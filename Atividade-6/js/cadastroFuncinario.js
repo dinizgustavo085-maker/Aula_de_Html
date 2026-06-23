d2fcac8cb4c1d0666084c332cce357c76313bde2
Gustavo Alves de Melo Diniz <dinizgustavo085@gmail.com>
2026-06-23 03:17:23 +0000

    implementando JSON ao cadastro de funcionario

--- Atividade-6/js/cadastroFuncinario.js
+++ Atividade-6/js/cadastroFuncinario.js
@@ -3,12 +3,21 @@ function cadastrofuncionario(){
     let hora = document.getElementById("horas-funcionario").value;
     let cargo = document.getElementById("cargo-funcionario").value;
 
+
     if (nome.trim() === "" || cargo.trim()  === "" ){
         alert("Por favor, coloque alguma informação");
         return;
     }
 
     console.log("Cadastando", nome, hora, cargo)
+    let objeto = {
+        nomefuncionario: nome,
+        horafuncionario: hora,
+        cargofuncionario: cargo
+    };
+
+    let textoJSON = JSON.stringify(objeto)
+    localStorage.setItem("dadosfuncionario", textoJSON)
 
     let sessaoLista = document.getElementById("lista-funcionarios");
     let cardFuncionario = document.createElement("div");
@@ -24,6 +33,7 @@ function cadastrofuncionario(){
         "<p><strong>Carga Horária:</strong> " + hora + "h semanais</p>";
 
     sessaoLista.appendChild(cardFuncionario);
+
 
     document.getElementById(nome).value = "";
     document.getElementById(cargo).value = "";
